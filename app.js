4217301cafd0347cd74fd80279ab096c5578dd6d
Aaron M <aaron.m.manley@gmail.com>
2017-06-28 14:24:47 -0700

    made minor response changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// Can also use $(function(){
-$(document).ready(function(){
+$(function(){
 
-// ---USER STATE (BE CAREFUL!)--- //
+// ---USER STATE (Be careful, don't DEATH STAR this!!!)--- //
+
 
   // Sets state in local storage based on defaults or called with userState
   const setState = (page="home", likes={}, articleData=null) => {
@@ -27,8 +27,8 @@ $(document).ready(function(){
   }
 
 
-
 // ---TEMPLATES--- //
+
  
   // Template for home page article population
   const articleComponent = (idx, headerImg, title, publish, preview) => 
@@ -61,25 +61,25 @@ $(document).ready(function(){
     $(".article-list-component")
       .empty()
       .append('<h1 class="text-center article-header">Articles</h1>')
-  // Ff articleData state is not set, will run GET request and set it
-  // refactor to do this in the constructor
+    // Ff articleData state is not set, will run GET request and set it
+    // refactor to do this in the constructor
     if (JSON.parse(localStorage.getItem("userState"))['articleData'] == null) {
       console.log("loading from server")
       $.getJSON('http://localhost:3000/articles', (r) => {
         $(".article-list-component").append()
-  // Creates item for each component in the article JSON blob
+      // Creates item for each component in the article JSON blob
         r.forEach((article, idx) => {
           $(".article-list-component").append(
             articleComponent(idx, article.headerImage, article.post_title, article.post_date, article.post_name)
           );
-  // Add click event handler to each item that will be used by articleDetailComponent
-  // to grab the right data from storage and render the detail page
+        // Add click event handler to each item that will be used by articleDetailComponent
+        // to grab the right data from storage and render the detail page
           $(`#${idx}`).click(articleDetail);
         });
-  // After render, sets state based on if we were home or not
+      // After render, sets state based on if we were home or not
         setState(userState['page'], userState['likes'], articleData=r)
       });
-  // else renders from saved articleData state
+    // else renders from saved articleData state
     } else {
       console.log("loading from local storage")
       let r = userState["articleData"]
@@ -117,17 +117,17 @@ $(document).ready(function(){
       .append(articleDetailComponent(articleData));
       $("button").click(indexPopulate);
       $("img").addClass("img-responsive");
-  // Set Glyphicon based on user state object
+    // Set Glyphicon based on user state object
       userState["likes"][idx] !== undefined ? liked = true : liked = false;
       if (liked) {
         $("#liked-button-container").find("span").removeClass("glyphicon-heart-empty").addClass("glyphicon-heart");
       }
-  // Toggle glyphicon appearence on click, change userState and setState to localStorage 
-  // in case of page reload or coming back to view later
+    // Toggle glyphicon appearence on click, change userState and setState to localStorage 
+    // in case of page reload or coming back to view later
       $("#liked-button-container").click(()=> {
         let icon = $(this).find("span")
         icon.toggleClass("glyphicon-heart-empty glyphicon-heart");
-  // If key exists, unlike/delete key, else set key to true; setState in both cases
+      // If key exists, unlike/delete key, else set key to true; setState in both cases
         if (userState["likes"][idx]) {
           delete userState["likes"][idx];
           setState(userState["page"], userState["likes"], userState["articleData"])
