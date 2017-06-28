38240ba3e6655eaa745c908a67facd15be69dd90
Aaron M <aaron.m.manley@gmail.com>
2017-06-28 12:24:36 -0700

    Single page loading from localStorage data

--- app.js
+++ app.js
@@ -1,27 +1,111 @@
 // Can also use $(function(){
 $(document).ready(function(){
 
-  const articleComponent = (title, publish, preview) => `
-    <div>
+// ---USER STATE (BE CAREFUL!)--- //
+
+  const setState = (page="home", likes=[], articleData=null) => {
+    let userState = {
+      page,
+      likes,
+      articleData
+    }
+
+    localStorage.setItem("userState", JSON.stringify(userState));
+  }
+
+  let userState = JSON.parse(localStorage.getItem("userState"));
+
+  if (userState == null) {
+    setState();
+    userState = JSON.parse(localStorage.getItem("userState"));
+  } else {
+    userState = userState;
+  }
+
+
+
+// ---TEMPLATES--- //
+ 
+  // Template for home page article population
+  const articleComponent = (idx, headerImg, title, publish, preview) => 
+    `<div>
       <h3>${title}</h3>
+      <img src=${headerImg} class="img-responsive"></img>
       <p>${publish}</p>
-      <p>${preview}</p>
+      <div>${preview}</div>
+      <button id=${idx}>Read more...</button>
     </div>
   `;
 
-  const indexPopulate = () => {
-    $(".article-list-component").
-      append('<h1 class="text-center article-header">Articles</h1>')
-    $.getJSON('http://localhost:3000/articles', (r) => {
-      $(".article-list-component").append()
-      r.forEach((article) => {
-        $(".article-list-component").append(
-          articleComponent(article.post_title, article.post_date, article.post_name)
-        );
-      });
-    });
-  }
+  // Template for article detail page population
+  const articleDetailComponent = (articleData) => 
+    `
+    <button><-- Go Back</button>
+    <h3>${articleData["post_title"]}</h3>
+    <h4>By ${articleData["post_author"]}</h4>
+    <p>Published ${articleData["post_date"]}</p>
+    <div>${articleData["post_content"]}</div>
+    <button><-- Go Back</button>
+    `
 
-  indexPopulate()
+
+// ---CONTROLS--- //
+
+  const indexPopulate = () => {
+    $(".article-list-component")
+      .empty()
+      .append('<h1 class="text-center article-header">Articles</h1>')
+      // if articleData state is not set, will run GET request and set it
+      // refactor to do this in the constructor
+    if (JSON.parse(localStorage.getItem("userState"))['articleData'] == null) {
+      console.log("loading from server")
+      $.getJSON('http://localhost:3000/articles', (r) => {
+        $(".article-list-component").append()
+        r.forEach((article, idx) => {
+          $(".article-list-component").append(
+            articleComponent(idx, article.headerImage, article.post_title, article.post_date, article.post_name)
+          );
+          $(`#${idx}`).click(articleDetail);
+        });
+        setState(userState['page'], userState['likes'], articleData=r)
+      });
+    // else renders from saved articleData state
+    } else {
+      console.log("loading from local storage")
+      let r = userState["articleData"]
+      $(".article-list-component").append()
+      r.forEach((article, idx) => {
+        $(".article-list-component").append(
+          articleComponent(idx, article.headerImage, article.post_title, article.post_date, article.post_name)
+        );
+        $(`#${idx}`).click(articleDetail);
+      });
+    };
+    if (userState["page"] !== "home") {
+      userState["page"] = "home";
+      setState(userState["page"], userState["likes"], userState["articleData"])
+    };
+  };
+
+
+  const articleDetail = (event, id) =>  {
+    let articleData;
+    if (id) {
+      articleData = userState["articleData"][id];
+    } else {
+      articleData = userState["articleData"][event.target.id];
+    }
+
+    $(".article-list-component").empty()
+      .append(articleDetailComponent(articleData));
+      $("button").click(indexPopulate);
+
+    userState["page"] = id || event.target.id
+    setState(userState["page"], userState["likes"], userState["articleData"])
+  };
+
+
+  userState["page"] === "home" ? indexPopulate() : articleDetail(event=null, id=userState["page"])
+
 
 });
